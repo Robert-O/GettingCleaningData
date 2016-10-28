99feec5f8a553559993d085d29d54b758705a4fc
Robert-O <robolden@gmail.com>
2016-10-28 16:49:16 -0400

    Updated README file

--- run_analysis.R
+++ run_analysis.R
@@ -10,38 +10,39 @@
 
 ## set working directory
 setwd("~/Rworkspace/Course_3/Peer assignment")
-
 ### Load and Merge the test and training sets
-## Step 1 - Load the training set 
+#### Load the training set of measurements
 training_set = read.table('./UCI HAR Dataset/train/X_train.txt')
 
-## Step 2 - Load the test set
+#### Load the test set of measurements
 test_set = read.table('./UCI HAR Dataset/test/X_test.txt')
 
-## Step 3 - Merge the test sets
+#### Merge the test and training set of measurements
 training_plus_test_set = rbind(test_set, training_set)
 
+
 ### Load and Merge the test and training set labels
-## Step 1 - Load the training set labels 
+#### Load the training set labels 
 training_set_labels = read.table('./UCI HAR Dataset/train/Y_train.txt')
 
-## Step 2 - Load the test set labels
+#### Load the test set labels
 test_set_labels = read.table('./UCI HAR Dataset/test/Y_test.txt')
 
-## Step 3 - Merge the training and test set labels
+#### Merge the training and test set labels
 merged_labels_set = rbind(test_set_labels, training_set_labels)
 
+
 ### Load and Merge the test and traing set subjects
-## Step 1 - Load the training set subjects 
+####Load the training set subjects 
 training_set_subjects = read.table('./UCI HAR Dataset/train/subject_train.txt')
 
-## Step 2 - Load the test set labels
+#### Load the test set labels
 test_set_subjects = read.table('./UCI HAR Dataset/test/subject_test.txt')
 
-## Step 3 - Merge the training and test set labels
+#### Merge the training and test set labels
 merged_subjects_set = rbind(test_set_subjects, training_set_subjects)
 
-### merge resuults, labels, and subjects
+### merge measurements, labels, and subjects
 superSet = cbind(training_plus_test_set, merged_labels_set, merged_subjects_set)
 
 
@@ -49,28 +50,29 @@ superSet = cbind(training_plus_test_set, merged_labels_set, merged_subjects_set)
 ### load the names of the features
 features = read.table('./UCI HAR Dataset/features.txt', stringsAsFactors = FALSE)
 
-## look for mean and standard deviation column names
+### look for mean and standard deviation column names
 meanOrStdColumns <- grep('mean|std', features$V2)
 meanOrStdColumnNames <- features[meanOrStdColumns, 'V2']
 
-## extract only columns for mean or std deviation. Plus, can't lose the activity
-## and subject columns, which are the last 2 columns. 
+### extract only columns for mean or std deviation. Plus, can't lose the activity
+### and subject columns, which are the last 2 columns. 
 activitySuperSetColumn = ncol(superSet) - 1
 subjectSuperSetColumn = ncol(superSet)
 mergedSetMeanStd <- superSet[, c(meanOrStdColumns, activitySuperSetColumn, subjectSuperSetColumn)]
 
-## load activity labels
-activity_labels = read.table('./UCI HAR Dataset/activity_labels.txt', 
-                             stringsAsFactors = FALSE)
 
 ## set activity label of merged data set to the description from the activty
 ## label table
+### load activity labels
+activity_labels = read.table('./UCI HAR Dataset/activity_labels.txt', 
+                             stringsAsFactors = FALSE)
+
+### replace activity identifiers with the acitivity labels
 count = 0
 activityColumn  = ncol(mergedSetMeanStd) - 1
 subjectColumn = ncol(mergedSetMeanStd)
 for (activity_row_value in mergedSetMeanStd[, activityColumn]){
       count = count + 1
-      cat('count: ', count, '\n', 'activity_row: ', activity_row, '\n' )
       activity_description = 
             activity_labels[activity_labels$V1 == activity_row_value, 'V2' ]
       mergedSetMeanStd[count, activityColumn] = activity_description
